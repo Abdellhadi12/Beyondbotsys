1946d5963035e79f5b1bbd007fe2e6607fc4fb1f
Abdellhadi12 <44175418+Abdellhadi12@users.noreply.github.com>
2018-12-16 22:08:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1630,14 +1630,13 @@ client.on("message", message => {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
  m.send(`${argresult}\n ${m}`);
 })
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
  message.delete(); 
 };     
 });
-
 
 
 
