adf127f0f71ad5f31b8630e183a8ecfce16a1cf6
Abdellhadi12 <44175418+Abdellhadi12@users.noreply.github.com>
2018-12-17 01:12:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3021,21 +3021,17 @@ client.on("guildMemberAdd", member => {
 
 
 
-
-
-
-
 client.on('message', message => {
  var prefix = "!"
     if(message.content.startsWith(prefix + 'new')) {
         let args = message.content.split(' ').slice(1).join(' ');
-        let support = message.guild.roles.find("name","Dead");
+        let support = message.guild.roles.find("name","night");
         let ticketsStation = message.guild.channels.find("name", "TICKETS");
         if(!args) {
             return message.channel.send('الرجاء كتابة سبب التذكرة');
         };
                 if(!support) {
-                    return message.channel.send('**Please make sure that `Dead` role exists and it\'s not duplicated.**');
+                    return message.channel.send('**Please make sure that `night` role exists and it\'s not duplicated.**');
                 };
             if(!ticketsStation) {
                 message.guild.createChannel("Ticket", "category");
@@ -3104,6 +3100,9 @@ client.on('message', message => {
             
     }
 });
+
+
+
 
 
 
